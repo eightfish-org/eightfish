8af131daf013d16785b2f83427f4f840605f258d
Daogang Tang <daogangtang@gmail.com>
2023-05-10 11:06:39 +0800

    clean.
    Signed-off-by: Daogang Tang <daogangtang@gmail.com>

--- build_docker_with_proxy_1st.sh
+++
@@ -1,6 +0,0 @@
-docker build \
-	--build-arg "http_proxy=http://172.17.128.1:7890" \
-	--build-arg "https_proxy=http://172.17.128.1:7890" \
-	--build-arg "NO_PROXY=localhost,127.0.0.1,.example.com" \
-	-f FirstStage.Dockerfile \
-	-t eightfish-m2-build . 
