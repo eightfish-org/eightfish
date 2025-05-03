8f306f1bf3e2c8fc705775e0f460783cb91aecbd
Daogang Tang <daogangtang@gmail.com>
2025-05-03 13:00:17 +0800

    add generalization param for EightFish handler.
    Signed-off-by: Daogang Tang <daogangtang@gmail.com>

--- sdk/src/handler.rs
+++ sdk/src/handler.rs
@@ -6,20 +6,21 @@ use crate::response::EightFishResponse as Response;
 
 /// All handlers should implement this Handler trait
 pub trait EightFishHandler: Send + Sync + Any {
-    fn handle(&self, req: &mut Request) -> Result<Response>;
+    fn handle<T: EightFishModel + Serialize>(&self, req: &mut Request) -> Result<Response<T>>;
 }
 
-impl<F> EightFishHandler for F
+impl<F, T> EightFishHandler for F
 where
-    F: Send + Sync + Any + Fn(&mut Request) -> Result<Response>,
+    F: Send + Sync + Any + Fn(&mut Request) -> Result<Response<T>>,
+    T: EightFishModel + Serialize,
 {
-    fn handle(&self, req: &mut Request) -> Result<Response> {
+    fn handle((&self, req: &mut Request) -> Result<Response<T>> {
         (*self)(req)
     }
 }
 
 impl EightFishHandler for Box<dyn EightFishHandler> {
-    fn handle(&self, req: &mut Request) -> Result<Response> {
+    fn handle<T: EightFishModel + Serialize>(&self, req: &mut Request) -> Result<Response<T>> {
         (**self).handle(req)
     }
 }
